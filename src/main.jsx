0cf81d26568d4cd91c3d4e3c5e916c84f65c5bf4
Juhye An <edujuhye@gmail.com>
2024-09-26 20:39:43 -0700

    Add routes and layouts

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ChatPage from './routes/chatPage/ChatPage.jsx';
+import DashboardPage from './routes/dashboardPage/Dashboard.jsx';
+import Homepage from './routes/homepage/Homepage.jsx';
+import RootLayout from "./layouts/rootLayout/rootLayout.jsx";
+import DashboardLayout from "./layouts/dashboardLayout/dashboardLayout.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    children:[
+      {
+        path:"/",
+        element: <Homepage />
+      },
+      {
+        element: <DashboardLayout />,
+        children:[
+          {
+            path:"/dashboard",
+            element: <DashboardPage />
+          },
+          {
+            path:"/dashboard/chats/:id",
+            element: <ChatPage />
+          },
+        ]
+      }
+
+    ]
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/routes/homepage/Homepage.jsx
@@ -0,0 +1,9 @@
+import './homepage.css'
+
+const Homepage = () => {
+  return (
+    <div className='homepage'><h1>Homepage</h1></div>
+  )
+}
+
+export default Homepage
